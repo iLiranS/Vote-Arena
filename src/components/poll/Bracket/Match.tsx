c2b94b6718b2a0aeddacac381bbf1dadbbe7d0ac
LiranS <lirangamerz@gmail.com>
2024-10-18 13:53:53 +0300

    made brackets even better

--- src/components/poll/Bracket/Match.tsx
+++ src/components/poll/Bracket/Match.tsx
@@ -4,22 +4,27 @@ import MatchItem from './MatchItem'
 
 const Match: React.FC<{ matchProps: bracketMatch, onActiveItemChange: (id: string) => void, activeItemTitle: string, prevStageMargin: number, stage: number, isLastStageMatch?: boolean }> = ({ matchProps, activeItemTitle, onActiveItemChange, prevStageMargin, stage, isLastStageMatch }) => {
 
+    const isFirstItemActive = activeItemTitle === matchProps.first.title;
+    const isSecondItemActive = activeItemTitle === matchProps.second.title;
+    // const isItemActive = isFirstItemActive || isSecondItemActive
+    // const isLoserHovered = matchProps.winnerTitle === activeItemTitle;
+
     return (
         <li className={`relative flex flex-col`}>
-            <MatchItem index={0} winnerTitle={matchProps.winnerTitle} activeItemTitle={activeItemTitle} onActiveItemChange={onActiveItemChange} itemProps={matchProps.first} />
-            <MatchItem index={1} winnerTitle={matchProps.winnerTitle} activeItemTitle={activeItemTitle} onActiveItemChange={onActiveItemChange} itemProps={matchProps.second} />
+            <MatchItem index={0} isLastStage={isLastStageMatch ?? false} winnerTitle={matchProps.winnerTitle} activeItemTitle={activeItemTitle} onActiveItemChange={onActiveItemChange} itemProps={matchProps.first} />
+            <MatchItem index={1} isLastStage={isLastStageMatch ?? false} winnerTitle={matchProps.winnerTitle} activeItemTitle={activeItemTitle} onActiveItemChange={onActiveItemChange} itemProps={matchProps.second} />
 
-            <div className={`absolute top-1/2  bg-border h-[2px] -translate-y-1/2 ${stage === 0 && !isLastStageMatch && 'left-0 w-[calc(100%_+26px)]'} ${stage !== 0 && !isLastStageMatch && ' left-0 w-[calc(100%_+26px)]'} ${isLastStageMatch && 'left-0 w-full'}`}></div>
+            {/* <div className={`absolute top-1/2 bg-border  h-[2px] -translate-y-1/2 ${stage === 0 && !isLastStageMatch && 'left-0 w-[calc(100%_+26px)]'} ${stage !== 0 && !isLastStageMatch && ' left-0 w-[calc(100%_+26px)]'} ${isLastStageMatch && 'left-0 w-full'}`}></div> */}
 
             {stage != 0 &&
                 <>
                     {/* upper part */}
-                    <div className='absolute top-1/4 -translate-y-1/2 h-[2px] w-[24px] -translate-x-full bg-border'></div>
-                    <div style={{ height: (prevStageMargin / 2) + 20 + 'px', translate: `0 calc(-100% + 20px)` }} className={`absolute -left-6 w-[2px] bg-border top-0 `}></div>
+                    <div className={`absolute top-1/4 -translate-y-1/2 h-[2px] w-[24px] -translate-x-full ${isFirstItemActive ? 'bg-violet-500' : 'bg-border'} `}></div>
+                    <div style={{ height: (prevStageMargin / 2) + 20 + 'px', translate: `0 calc(-100% + 20px)` }} className={`absolute -left-6 w-[2px] ${isFirstItemActive ? 'bg-violet-500' : 'bg-border'} top-0 `}></div>
 
                     {/* lower part */}
-                    <div className='absolute bottom-1/4 -translate-y-1/2 h-[2px] w-[24px] -translate-x-full bg-border'></div>
-                    <div style={{ height: (prevStageMargin / 2) + 22 + 'px', translate: `0 calc(100% - 22px)` }} className={`absolute -left-6 w-[2px] bg-border bottom-0 `}></div>
+                    <div className={`absolute bottom-1/4 -translate-y-1/2 h-[2px] w-[24px] -translate-x-full ${isSecondItemActive ? 'bg-violet-500' : 'bg-border'}`}></div>
+                    <div style={{ height: (prevStageMargin / 2) + 22 + 'px', translate: `0 calc(100% - 22px)` }} className={`absolute -left-6 w-[2px] ${isSecondItemActive ? 'bg-violet-500' : 'bg-border'} bottom-0 `}></div>
                 </>
             }
         </li>
--- src/components/poll/Bracket/MatchItem.tsx
+++ src/components/poll/Bracket/MatchItem.tsx
@@ -1,18 +1,38 @@
 import { optionPollForm } from '@/lib/models';
+import Link from 'next/link';
 import React from 'react'
 
-const MatchItem: React.FC<{ itemProps: optionPollForm, winnerTitle: string, activeItemTitle: string, onActiveItemChange: (id: string) => void, index: number }> = ({ itemProps, onActiveItemChange, activeItemTitle, winnerTitle, index }) => {
+const MatchItem: React.FC<{ itemProps: optionPollForm, winnerTitle: string, activeItemTitle: string, onActiveItemChange: (id: string) => void, index: number, isLastStage: boolean }> = ({ itemProps, onActiveItemChange, activeItemTitle, winnerTitle, index, isLastStage }) => {
     const didLose = winnerTitle.length > 0 && winnerTitle !== itemProps.title;
     const isActive = activeItemTitle === itemProps.title;
+
+    const itemHoverHandler = () => {
+        if (itemProps.title === 'TBD') return;
+        onActiveItemChange(itemProps.title)
+    }
+    const itemHoverOutHandler = () => {
+        onActiveItemChange('')
+    }
+
     return (
-        <div onClick={() => { if (itemProps.title === 'TBD') return; onActiveItemChange(itemProps.title) }}
-            className={` w-40 aspect-[4/1] border-[2px] ${index === 0 && 'border-b-0'} ${index === 1 && 'border-t-0'} overflow-hidden  ${isActive && 'bg-violet-500'} `}>
-            <section className={`flex items-center  ${didLose && 'bg-slate-600/50 opacity-50'} w-full h-full pl-2`}>
-                <p className={`truncate ${didLose && 'opacity-60'}`}>{itemProps.title}</p>
-                <p>{itemProps.votes ?? ''}</p>
+        <div onMouseEnter={itemHoverHandler} onMouseLeave={itemHoverOutHandler}
+            className={` w-40 aspect-[4/1] border-[2px] ${didLose && 'bg-slate-600/50 opacity-70'} ${index === 0 && 'border-b-0'} ${index === 1 && 'border-t-0'}   ${isActive && 'bg-violet-500'} relative`}>
+            <section className={`flex items-center   w-full h-full`}>
+                <ItemSection title={itemProps.title} src={itemProps.src.length > 1 ? itemProps.src : undefined} isActive={isActive} votes={itemProps.votes} />
             </section>
+            {!isLastStage && !didLose &&
+                <div className={`absolute h-[2px]  w-[calc(100%_+28px)] left-0 ${isActive && winnerTitle.length > 1 ? 'bg-violet-500 z-10' : 'bg-border'} ${index === 0 ? 'bottom-0' : 'top-0'}`}></div>
+            }
+
         </div>
     )
+}
+
+const ItemSection: React.FC<{ title: string, votes?: number, src?: string, isActive: boolean }> = ({ title, votes, src, isActive }) => {
+    return <div className={`grid pl-2 justify-between items-center gap-2 h-full w-full truncate ${votes ? 'grid-cols-[1fr,max-content]' : 'grid-cols-1'}`}>
+        {src ? <Link href={src} className='truncate'>{title}</Link> : <p className='truncate'>{title}</p>}
+        {votes && <section className={` ${isActive ? 'bg-violet-500 border-l-2 border-violet-700' : 'bg-border'} grid place-items-center w-8 h-full text-sm`}>{votes}</section>}
+    </div>
 }
 
 export default MatchItem
