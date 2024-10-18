febf9e8ff7de1f73a479a49a2c4d9e6150902c00
LiranS <lirangamerz@gmail.com>
2024-10-18 16:07:25 +0300

    fixed ios related bracket visual bracket bug

--- src/components/poll/Bracket/MatchItem.tsx
+++ src/components/poll/Bracket/MatchItem.tsx
@@ -1,6 +1,8 @@
 import { optionPollForm } from '@/lib/models';
 import Link from 'next/link';
 import React from 'react'
+import { CiLink } from "react-icons/ci";
+
 
 const MatchItem: React.FC<{ itemProps: optionPollForm, winnerTitle: string, activeItemTitle: string, onActiveItemChange: (id: string) => void, index: number, isLastStage: boolean }> = ({ itemProps, onActiveItemChange, activeItemTitle, winnerTitle, index, isLastStage }) => {
     const didLose = winnerTitle.length > 0 && winnerTitle !== itemProps.title;
@@ -16,7 +18,7 @@ const MatchItem: React.FC<{ itemProps: optionPollForm, winnerTitle: string, acti
 
     return (
         <div onMouseEnter={itemHoverHandler} onMouseLeave={itemHoverOutHandler}
-            className={` w-40 aspect-[4/1] border-[2px]  ${index === 0 && 'border-b-0'} ${index === 1 && 'border-t-0'}   ${isActive && 'bg-violet-500 border-violet-500'} relative`}>
+            className={` w-40 h-10 border-[2px]  ${index === 0 && 'border-b-0'} ${index === 1 && 'border-t-0'}   ${isActive && 'bg-violet-500 border-violet-500'} relative`}>
             <section className={`flex items-center   w-full h-full ${didLose && 'bg-border opacity-70'}`}>
                 <ItemSection title={itemProps.title} src={itemProps.src.length > 1 ? itemProps.src : undefined} isActive={isActive} votes={itemProps.votes} />
             </section>
@@ -34,7 +36,15 @@ const MatchItem: React.FC<{ itemProps: optionPollForm, winnerTitle: string, acti
 
 const ItemSection: React.FC<{ title: string, votes?: number, src?: string, isActive: boolean }> = ({ title, votes, src, isActive }) => {
     return <div className={`grid pl-2 justify-between items-center gap-2 h-full w-full truncate ${votes ? 'grid-cols-[1fr,max-content]' : 'grid-cols-1'}`}>
-        {src ? <Link href={src} className='truncate max-w-max'>{title}</Link> : <p className='truncate'>{title}</p>}
+        {src ?
+            <div className={`w-full justify-between grid grid-cols-[1fr,max-content] items-center ${!votes && 'pr-2'}`}>
+                <p title={title} className='truncate'>{title}</p>
+                <Link target='_blank' className='opacity-70' href={src}>
+                    <CiLink />
+                </Link>
+            </div>
+
+            : <p className='truncate'>{title}</p>}
         {votes && <section className={` ${isActive ? 'bg-violet-500 border-l-2 border-violet-700' : 'bg-border'} grid place-items-center w-8 h-full text-sm`}>{votes}</section>}
     </div>
 }
